e151837d9b10266960566c8b2f596446e72cc0b2
JoaoVitorNY <joaovitornakahodo@gmail.com>
2020-07-02 22:19:41 -0300

    Commit - Ajustando o começo

--- banco/config.php
+++ banco/config.php
@@ -6,6 +6,3 @@ define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASSWORD', '');
 define('DBNAME', 'dbaulads');
-define('TYPE_MSG_SUCCESS', 'success');
-define('TYPE_MSG_ERROR', 'error');
-define('TYPE_MSG_INFO', 'info');
--- src/categorias/modelo/create-categoria.php
+++ src/categorias/modelo/create-categoria.php
@@ -4,40 +4,41 @@ include('../../banco/conexao.php');
 
 if(!$conexao){
     $dados = array(
-        'tipo' -> TYPE_MSG_INFO,
-        'mensagem' -> 'Não foi possível conectar ao banco de dados...'
+        'tipo' => 'info',
+        'mensagem' => 'Não foi possível conectar ao banco de dados...'
     );
 
 }else{
     $requestData = $_REQUEST;
-    $ativo = 
     //$requestData = array_map('utf8_decode', $requestData);
-    $requestData['ativo'] = $requestData['ativo'] == "on" ? "S" : "N";
-    $requestData['categoria'] = date('Y-d-m H:i:s', strtotime($requestData['dataagora']));
 
     if(empty($requestData['nome']) && empty($requestData['ativo'])){
         $dados = array(
-            'tipo' => TYPE_MSG_INFO,
+            'tipo' => 'info',
             'mensagem' => 'Existem campos obrigatórios vazios...'
         );
     }else{
+        $requestData['ativo'] = $requestData['ativo'] == "on" ? "S" : "N";
+        $requestData['categoria'] = date('Y-d-m H:i:s', strtotime($requestData['dataagora']));
+        $requestData['dataagora'] = date_format($date, 'Y-m-d H:i:s');
+        
         $sqlComando = "INSERT INTO CATEGORIAS(nome, ativo, datacriacao, datamodificacao) 
-        VALUES('$requestData['nome']', '$requestData['ativo']', '$requestData['dataagora']', '$requestData['dataagora']')";
+        VALUES('$requestData[nome]', '$requestData[ativo]', '$requestData[dataagora]', '$requestData[dataagora]')";
 
         $resultado = mysqli_query($conexao, $sqlComando);
 
         if(!$resultado){
             $dados = array(
-                'tipo' => TYPE_MSG_SUCCESS,
+                'tipo' => 'success',
                 'mensagem' => 'Categoria cadastrada com sucesso!'
             );
         }else{
             $dados = array(
-                'tipo' => TYPE_MSG_ERROR,
+                'tipo' => 'error',
                 'mensagem' => 'Não foi possível cadastrar categoria.'               
             );
         }      
     }
     mysqli_close($conexao);
 }
-echo json_encode($dados);
+echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
